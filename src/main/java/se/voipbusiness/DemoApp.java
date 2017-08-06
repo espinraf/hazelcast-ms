49595b16a68474a4f1d2ecb3c63db5f138c35fe6
Rafael Espino <rafael.espino@gmail.com>
2017-08-06 15:27:06 +0200

    Added Support to Spring boot Admin Server

--- src/main/java/se/voipbusiness/DemoApp.java
+++ src/main/java/se/voipbusiness/DemoApp.java
@@ -10,6 +10,7 @@ public class DemoApp {
 
     public static void main(String[] args) {
 
+        System.out.println(args);
         SpringApplication.run(DemoApp.class, args);
 
     }
--- src/main/java/se/voipbusiness/ms/TestWebController.java
+++ src/main/java/se/voipbusiness/ms/TestWebController.java
@@ -1,11 +1,15 @@
 package se.voipbusiness.ms;
 
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.api.ContentResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.io.IOException;
 
 /**
  * Created by espinraf on 2017-07-19.
@@ -22,6 +26,18 @@ public class TestWebController {
     public String index() {
         log.info("Serving Static Pages....");
         webSocketHandler.sendToAll("Greetings from Web controller");
+
+        ContentResponse response = null;
+
+        try {
+            HttpClient httpc = new HttpClient();
+            httpc.start();
+            response = httpc.GET("http://11.1.0.3:9090/caching/2");
+            webSocketHandler.sendToAll(response.getContentAsString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return "controller.html";
     }
 }
