abaf9b67f70157ac0333c0353f8afe4ed0886f8e
Rafael Espino <rafael.espino@gmail.com>
2017-08-13 14:50:16 +0200

    Minor changes

--- src/main/java/se/voipbusiness/DemoApp.java
+++ src/main/java/se/voipbusiness/DemoApp.java
@@ -3,8 +3,10 @@ package se.voipbusiness;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+//@EnableScheduling
 //@ComponentScan(basePackages={"se.voipbusiness.batch", "se.voipbusiness.ms"})
 public class DemoApp {
 
